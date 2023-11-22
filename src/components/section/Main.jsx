56e9e5b307e7c13343fa588c96a20d52d9999c7c
catgeot <catgeot@naver.com>
2023-11-23 07:56:52 +0900

    메인콘텐츠 영역

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
+import Search from './Search'
 const Main = (props) => {
   return (
     <HelmetProvider>
-       <Helmet 
-          titleTemplate="%s | Webs Youtube" 
-          defaultTitle="Webs Youtube" 
-          defer={false}
-      >
-          {props.title && <title>{props.title}</title>}
-          <meta name="description" content={props.description} />
-        </Helmet>
+      <Helmet 
+        titleTemplate="%s | Webs Youtube" 
+        defaultTitle="Webs Youtube" 
+        defer={false}
+    >
+        {props.title && <title>{props.title}</title>}
+        <meta name="description" content={props.description} />
+      </Helmet>
 
-        <Header />
-          <main id='main' role='main'>
-            {props.children}
-          </main>
-        <Footer />
+      <Header />
+        <main id='main' role='main'>
+          <Search />
+          {props.children}
+        </main>
+      <Footer />
     </HelmetProvider>
 
       
