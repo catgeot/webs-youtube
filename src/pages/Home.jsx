ab4be469bdf903f8efcc8eed9f908b945dec5804
catgeot <catgeot@naver.com>
2023-11-29 07:43:16 +0900

    비디오 컴포넌트 통합작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,29 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Webd from '../components/contents/Webd'
-import Website from '../components/contents/Website'
-import Gsap from '../components/contents/Gsap'
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
+import VideoSlider from '../components/videos/VideoSlider'
+
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { gsapText } from '../data/gsap'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
 
 const Home = () => {
-  return (
-    <Main 
-      title="포트폴리오" 
-      description="포트폴리오에 오신것을 환영합니다."
-    >
-      <Today />
-      <Developer />     
-      <Webd /> 
-      <Website />
-      <Gsap />
-      <Portfolio />
-      <Youtube />
-    </Main>  
-  )
+     return (
+        <Main 
+            title="포트폴리오" 
+            description="포트폴리오에 오신것을 환영합니다."
+        >
+            <Today />
+            <Developer />     
+            <VideoSlider videos={webdText} title='웹디자인 기능사 준비는 이걸로 끝!' id='webd' /> 
+            <VideoSlider videos={websiteText} title='웹표준 사이트 튜토리얼 강의입니다.' id='website' /> 
+            <VideoSlider videos={gsapText} title='GSAP 사이트 튜토리얼 강의입니다.' id='gsap' /> 
+            <VideoSlider videos={portfolioText} title='포트폴리오 사이트 튜토리얼 강의입니다.' id='portfolio' /> 
+            <VideoSlider videos={youtubeText} title='유튜브 사이트 튜토리얼 강의입니다.' id='youtube' />             
+        </Main>  
+    )
 }
 
 export default Home
