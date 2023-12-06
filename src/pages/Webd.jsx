1e155ce44795006367198e545d0127d99e7a5068
catgeot <catgeot@naver.com>
2023-12-07 08:04:04 +0900

    test

--- src/pages/Webd.jsx
+++ src/pages/Webd.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import VideoCard from '../components/videos/VideoCard'
+
+import { webdText } from '../data/webd'
 
 const Webd = () => {
     return (
         <Main 
             title = "웹디자인 기능사"
             description="웹디자인 기능사 튜토리얼 강의입니다.">
-            Webd
+            
+            <section id='webdPage'>
+                <h2>😄 웹디자인기능사 한번에 따자!</h2>
+                <div className="video_inner">
+                    <VideoCard videos={webdText} />
+                </div>
+            </section>
         </Main>
     )
 }
