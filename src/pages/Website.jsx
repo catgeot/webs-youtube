3e8cb1f698fde12121727fc8eaae762af0bbefab
catgeot <catgeot@naver.com>
2023-12-06 16:32:30 +0900

    나머지 페이지 작업

--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
+import VideoCard from '../components/videos/VideoCard'
+
+import { websiteText } from '../data/website'
+
 
 const Website = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 300);
+    }, []);
+
+    const websitePageClass = loading ? 'isLoading' : 'isloaded'
     return (
         <Main 
             title = "웹표준 사이트"
             description="웹표준 사이트 튜토리얼 강의입니다.">
-            Website
+            
+            <section id='websitePage' className={websitePageClass}>
+                <h2>🤬웹표준 사이트 강의의 강자!</h2>
+                <div className="video_inner">
+                    <VideoCard videos={websiteText} />
+                </div>
+            </section>
         </Main>
     )
 }
